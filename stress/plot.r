c768e281800eca54dc12ce42be189e1e1c3788de
Michael Neumann <mneumann@ntecs.de>
2022-11-04 22:31:29 +0100

    Refactor plot script

--- stress/plot.r
+++ stress/plot.r
@@ -1,59 +1,17 @@
-data1 <- read.csv("data_x1.csv", header=TRUE)
+xs <- list(1, 2, 5, 10)
 
-png(file = "ttfb_x1.png")
-boxplot(ttfb ~ clients, data = data1, xlab = "# Clients",
-   ylab = "Time to first byte", main = "Benchmark (x1)")
-dev.off()
+for (x in xs) {
+	data1 <- read.csv(sprintf("data_x%s.csv", x), header=TRUE)
 
-png(file = "ttc_x1.png")
-boxplot(ttc ~ clients, data = data1, xlab = "# Clients",
-   ylab = "Time to completion", main = "Benchmark (x1)")
-dev.off()
+	png(file = sprintf("ttfb_x%s.png", x))
+	boxplot(ttfb ~ clients, data = data1, xlab = "# Clients",
+	ylab = "Time to first byte", main = sprintf("Benchmark (%s)", x))
+	dev.off()
 
+	png(file = sprintf("ttc_x%s.png", x))
+	boxplot(ttc ~ clients, data = data1, xlab = "# Clients",
+	ylab = "Time to completion", main = sprintf("Benchmark (%s)", x))
+	dev.off()
+}
 
-
-
-
-data1 <- read.csv("data_x2.csv", header=TRUE)
-
-png(file = "ttfb_x2.png")
-boxplot(ttfb ~ clients, data = data1, xlab = "# Clients",
-   ylab = "Time to first byte", main = "Benchmark (x2)")
-dev.off()
-
-png(file = "ttc_x2.png")
-boxplot(ttc ~ clients, data = data1, xlab = "# Clients",
-   ylab = "Time to completion", main = "Benchmark (x2)")
-dev.off()
-
-
-
-
-data1 <- read.csv("data_x5.csv", header=TRUE)
-
-png(file = "ttfb_x5.png")
-boxplot(ttfb ~ clients, data = data1, xlab = "# Clients",
-   ylab = "Time to first byte", main = "Benchmark (x5)")
-dev.off()
-
-png(file = "ttc_x5.png")
-boxplot(ttc ~ clients, data = data1, xlab = "# Clients",
-   ylab = "Time to completion", main = "Benchmark (x5)")
-dev.off()
-
-
-
-
-data1 <- read.csv("data_x10.csv", header=TRUE)
-
-png(file = "ttfb_x10.png")
-boxplot(ttfb ~ clients, data = data1, xlab = "# Clients",
-   ylab = "Time to first byte", main = "Benchmark (x10)")
-dev.off()
-
-png(file = "ttc_x10.png")
-boxplot(ttc ~ clients, data = data1, xlab = "# Clients",
-   ylab = "Time to completion", main = "Benchmark (x10)")
-dev.off()
-
-
+# head(subset(data1, data1$clients == 50))
