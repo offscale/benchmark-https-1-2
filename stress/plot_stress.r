9d11a0427011ed85ba170cd343ed6f505845d313
Michael Neumann <mneumann@ntecs.de>
2022-11-05 11:17:37 +0100

    Slightly change the stress plot

--- stress/plot_stress.r
+++ stress/plot_stress.r
@@ -3,9 +3,17 @@ data <- read.csv("results/data_stress.csv", header=TRUE)
 
 pdf(file = "results/stress.pdf")
 par(mfrow = c(2, 1))
-hist(data$ttfb, breaks=100, xlim=c(0,2), col='blue',
-     xlab="Time to first byte [s]", main = "Stress test using 200 concurrent clients sustained over ~1h")
+hist(data$ttfb * 1000,
+     breaks='Scott',
+     xlim=c(0,1000),
+     col='blue',
+     xlab="Time to first byte [ms]",
+     main = "Stress test using 200 concurrent clients sustained over ~1h")
 
-hist(data$ttc, breaks=100, xlim=c(0,2), col='green',
-     xlab="Time to completion [s]", main = "")
+hist(data$ttc * 1000,
+     breaks='Scott',
+     xlim=c(0,2500),
+     col='green',
+     xlab="Time to completion [ms]",
+     main = "")
 dev.off()
