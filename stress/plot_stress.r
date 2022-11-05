136cc8abfb483c936f828c72c59924178f5213e0
Michael Neumann <mneumann@ntecs.de>
2022-11-05 10:57:54 +0100

    Change colors of histogram

--- stress/plot_stress.r
+++ stress/plot_stress.r
@@ -3,9 +3,9 @@ data <- read.csv("results/data_stress.csv", header=TRUE)
 
 pdf(file = "results/stress.pdf")
 par(mfrow = c(2, 1))
-hist(data$ttfb, breaks=100, xlim=c(0,2), col='black',
+hist(data$ttfb, breaks=100, xlim=c(0,2), col='blue',
      xlab="Time to first byte [s]", main = "Stress test using 200 concurrent clients sustained over ~1h")
 
-hist(data$ttc, breaks=100, xlim=c(0,2), col='blue',
+hist(data$ttc, breaks=100, xlim=c(0,2), col='green',
      xlab="Time to completion [s]", main = "")
 dev.off()
